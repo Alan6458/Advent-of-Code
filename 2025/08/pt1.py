461661502400f63ad65defafbfa6d0d5bb33227d
Alan <88736896+Alan6458@users.noreply.github.com>
2025-12-07 21:35:53 -0800

    2025 day 8

---
+++ 2025/08/pt1.py
@@ -0,0 +1,51 @@
+inp = """
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
+"""
+
+inp = [i.split(",") for i in inp.split("\n") if i]
+inp = [(int(a), int(b), int(c)) for a, b, c in inp]
+li = 10 if len(inp) != 1000 else 1000
+
+l = []
+
+for i in range(len(inp) - 1):
+    for j in range(i + 1, len(inp)):
+        l.append([((inp[i][0] - inp[j][0]) ** 2 + (inp[i][1] - inp[j][1]) ** 2 + (inp[i][2] - inp[j][2]) ** 2) ** 0.5, i, j])
+
+l = sorted(l)
+
+sl = [set([i]) for i in range(len(inp))]
+d = {i:i for i in range(len(inp))}
+
+for ind in range(li):
+    i, j = l[ind][1], l[ind][2]
+    fi = d[j]
+    if fi == d[i]:
+        continue
+    for k in sl[d[j]]:
+        d[k] = d[i]
+    sl[d[i]] = sl[d[i]].union(sl[fi])
+    sl[fi] = set()
+
+l = sorted([len(i) for i in sl], reverse=True)
+
+print(l[0] * l[1] * l[2])
